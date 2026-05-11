ae6118a5eaed98c4ce1aa52169921869f79a5a06
GOB <goblin52@gmail.com>
2026-05-11 15:03:18 +0900

    Add UnitRFID2 (M5Unit-RFID) support to NFCB examples

--- examples/UnitUnified/NFCB/Detect/Detect.ino
+++ examples/UnitUnified/NFCB/Detect/Detect.ino
@@ -4,16 +4,19 @@
  * SPDX-License-Identifier: MIT
  */
 /*
-  Example using M5UnitUnified for ST25R3916
+  Example using M5UnitUnified for M5Unit-NFC/RFID
   Detect NFC-B PICC
+  This example is shared with M5Unit-RFID
 */
 // *************************************************************
 // Choose one define symbol to match the unit you are using
 // *************************************************************
-#if !defined(USING_UNIT_NFC) && !defined(USING_CAP_CC1101)
+#if !defined(USING_UNIT_NFC) && !defined(USING_CAP_CC1101) && !defined(USING_UNIT_RFID2)
 // For UnitNFC
 // #define USING_UNIT_NFC
 // For CapNFC
 // #define USING_CAP_CC1101
+// For UnitRFID2
+// #define USING_UNIT_RFID2
 #endif
 #include "main/Detect.cpp"
--- examples/UnitUnified/NFCB/Detect/main/Detect.cpp
+++ examples/UnitUnified/NFCB/Detect/main/Detect.cpp
@@ -4,23 +4,32 @@
  * SPDX-License-Identifier: MIT
  */
 /*
-  Example using M5UnitUnified for M5Cardputer-ADV with CapCC1101
+  Example using M5UnitUnified for M5Unit-NFC/RFID
   Detect NFC-B PICC
+  This example is shared with M5Unit-RFID
 */
 #include <M5Unified.h>
 #include <M5UnitUnified.h>
 #include <M5UnitUnifiedNFC.h>
 #include <M5Utility.h>
+#include <Wire.h>
+#include <M5HAL.hpp>  // For NessoN1
 #include <vector>
 
 // *************************************************************
 // Choose one define symbol to match the unit you are using
 // *************************************************************
-#if !defined(USING_UNIT_NFC) && !defined(USING_CAP_CC1101)
+#if !defined(USING_UNIT_NFC) && !defined(USING_CAP_CC1101) && !defined(USING_UNIT_RFID2)
 // For UnitNFC
 // #define USING_UNIT_NFC
 // For CapNFC
 // #define USING_CAP_CC1101
+// For UnitRFID2
+// #define USING_UNIT_RFID2
+#endif
+
+#if defined(USING_UNIT_RFID2)
+#include <M5UnitUnifiedRFID.h>
 #endif
 
 using namespace m5::nfc;
@@ -36,6 +45,9 @@ m5::unit::UnitNFC unit{};  // I2C
 #elif defined(USING_CAP_CC1101)
 #pragma message "Choose CapCC1101NFC"
 m5::unit::CapCC1101NFC unit{};  // CapCC1101 (SPI)
+#elif defined(USING_UNIT_RFID2)
+#pragma message "Choose UnitRFID2"
+m5::unit::UnitRFID2 unit{};  // UnitRFID2 (M5Unit-RFID, WS1850S)
 #else
 #error Choose unit please!
 #endif
@@ -47,14 +59,21 @@ void setup()
     M5.begin();
     M5.setTouchButtonHeightByRatio(100);
 
+    // Select NFC-B mode via config() (applied at begin())
     auto cfg = unit.config();
     cfg.mode = NFC::B;
     unit.config(cfg);
 
-#if defined(USING_UNIT_NFC)
+#if defined(USING_UNIT_NFC) || defined(USING_UNIT_RFID2)
+    // NessoN1: Arduino Wire (I2C_NUM_0) cannot be used for GROVE port.
+    //   Wire is used by M5Unified In_I2C for internal devices.
+    // NanoC6: Wire.begin() on GROVE pins conflicts with Ex_I2C on the same I2C_NUM_0.
     auto board = M5.getBoard();
     bool unit_ready{};
-    // NessoN1: SoftwareI2C too slow for NFC RF timing -> use port_a (Wire) via else branch
+#if defined(USING_M5DIAL_BUILTIN_WS1850S)
+    M5_LOGI("Using M5.In_I2C for builtin WS1850S");
+    unit_ready = Units.add(unit, M5.In_I2C) && Units.begin();
+#else
     if (board == m5::board_t::board_M5NanoC6) {
         M5_LOGI("Using M5.Ex_I2C");
         unit_ready = Units.add(unit, M5.Ex_I2C) && Units.begin();
@@ -66,6 +85,7 @@ void setup()
         Wire.begin(pin_num_sda, pin_num_scl, 400 * 1000U);
         unit_ready = Units.add(unit, Wire) && Units.begin();
     }
+#endif  // USING_M5DIAL_BUILTIN_WS1850S
     if (!unit_ready) {
         M5_LOGE("Failed to begin");
         lcd.fillScreen(TFT_RED);
@@ -91,6 +111,7 @@ void setup()
         }
     }
 #endif
+
     M5_LOGI("M5UnitUnified initialized");
     M5_LOGI("%s", Units.debugInfo().c_str());
 
